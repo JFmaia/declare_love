af29d8e868adc72c2e903a4e63dd0f12c45f0dbc
JFmaia <jfmaia.dev@gmail.com>
2024-01-07 09:06:28 -0300

    feat: Adicionando primeiro test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import Carrosel from '@/components/MyCarrossel.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('Testando o Carrosel', () => {
+  it('Se existe tal frase no componente', () => {
+    const wrapper = mount(Carrosel)
+    expect(wrapper.html()).toContain('Oi novamente, bem aqui você saberá algumas coisas do meu coração, coisas que eu não te contei.')
   })
 })
